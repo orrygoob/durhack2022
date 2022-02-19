57020efbd8e21979649fb2fba93558e7d9323952
Orry Gooberman <orry.gooberman@durham.ac.uk>
2022-02-19 15:25:57 +0000

    Basic websocket test

--- server/app.js
+++ server/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
-const app = express();
+var expressWs = require('express-ws');
+
+var expressWs = expressWs(express());
+var app = expressWs.app;
 
 app.use(express.static('client'));
+
+var myWebsocket = expressWs.getWss('/');
+
+app.ws('/', function(ws, req) {
+    ws.onmessage = function(msg) {
+        console.log(msg.data);
+        myWebsocket.clients.forEach(function (client) {
+            client.send(msg.data + "somefrf");
+        });
+    };
+});
 
 module.exports = app;
