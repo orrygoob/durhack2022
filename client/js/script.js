e476248cd0e9b4a879d50f902d94f4dd88180577
Amren Stephenson <92263829+amrenstephenson@users.noreply.github.com>
2022-02-20 07:45:21 +0000

    async

--- client/js/script.js
+++ client/js/script.js
@@ -105,7 +105,7 @@ function onLogin () {
 	});
 
 	socket = new WebSocket('ws://' + window.location.host);
-	socket.onopen = () => {
+	socket.onopen = async () => {
 		socket.send(JSON.stringify({ playerID: -1, x: playerX, y: playerY, dx: 0, dy: 0, name: getUsername(), tint: getUserTint() }));
 	};
 
